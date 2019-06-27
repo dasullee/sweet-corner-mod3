f82d81b31c5701fd9b1238398de6560b9541166a
Dasul Lee <nicoledasullee@gmail.com>
2019-06-26 21:02:32 -0700

    Finished Feature Set 1

---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './header.scss'
+
+export default () =>{
+    return (
+        <div>
+            <h1 className="center">This is the Header.</h1>
+        </div>
+    )
+}
